3ab69b9c7f44137a45fad27199689df8f9b556cd
kenjiru <kenjiru.ro@gmail.com>
2015-11-29 21:57:46 +0100

    Use the new Toolbar with AppCompat-v7.

--- app/src/main/java/ro/kenjiru/app/activities/RichTextEditorActivity.java
+++ app/src/main/java/ro/kenjiru/app/activities/RichTextEditorActivity.java
@@ -1,48 +1,63 @@
 package ro.kenjiru.app.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import ro.kenjiru.ui.widget.richtexteditor.R;
 import ro.kenjiru.ui.widget.richtexteditor.RichTextEditor;
 
 
-public class RichTextEditorActivity extends Activity {
+public class RichTextEditorActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rich_text_editor);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
         RichTextEditor editor = (RichTextEditor) findViewById(R.id.editor);
 //        editor.enableContextOptions();
         editor.enableActionModes();
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rich_text_editor, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_toggle_format_toolbar) {
+            toggleFormattingToolbar();
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void toggleFormattingToolbar() {
+        View toolbar = findViewById(R.id.format_toolbar);
+
+        if (toolbar.getVisibility() == View.VISIBLE) {
+            toolbar.setVisibility(View.GONE);
+        } else {
+            toolbar.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
