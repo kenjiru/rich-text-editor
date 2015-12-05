aae23cd49b9d0295f600a8791819d4c13afd7dc6
kenjiru <kenjiru.ro@gmail.com>
2015-12-05 23:31:47 +0100

    Use a different menu for each activity.

--- app/src/main/java/ro/kenjiru/app/activities/CheckBoxGroupActivity.java
+++ app/src/main/java/ro/kenjiru/app/activities/CheckBoxGroupActivity.java
@@ -55,7 +55,7 @@ public class CheckBoxGroupActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.checkbox_group_menu, menu);
         return true;
     }
 
--- app/src/main/java/ro/kenjiru/app/activities/RichTextEditorActivity.java
+++ app/src/main/java/ro/kenjiru/app/activities/RichTextEditorActivity.java
@@ -32,7 +32,7 @@ public class RichTextEditorActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.rich_text_editor_menu, menu);
         return true;
     }
 
