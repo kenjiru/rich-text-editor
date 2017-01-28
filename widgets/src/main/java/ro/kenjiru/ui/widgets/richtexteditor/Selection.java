63ac5eff36ac692652ce0c1e4f12b512d530c2d3
kenjiru <kenjiru.ro@gmail.com>
2017-01-28 22:47:02 +0100

    Selection can extend to full lines and detect the paragraphs touched by it.

--- widgets/src/main/java/ro/kenjiru/ui/widgets/richtexteditor/Selection.java
+++ widgets/src/main/java/ro/kenjiru/ui/widgets/richtexteditor/Selection.java
@@ -2,11 +2,14 @@ package ro.kenjiru.ui.widgets.richtexteditor;
 
 import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Selection {
     public int start;
     public int end;
 
-    Selection(int start, int end) {
+    public Selection(int start, int end) {
         this.start = start;
         this.end = end;
 
@@ -22,11 +25,52 @@ public class Selection {
         this(editor.getSelectionStart(), editor.getSelectionEnd());
     }
 
+    public List<Selection> getParagraphsInSelection(CharSequence str) {
+        List<Selection> paragraphs = new ArrayList<>();
+
+        Selection wholeLinesSelection = this.extendToFullLines(str);
+        int startOfParagraph = wholeLinesSelection.start;
+        int endOfParagraph = wholeLinesSelection.start;
+
+        while(endOfParagraph < str.length()) {
+            if (str.charAt(endOfParagraph) == '\n' || endOfParagraph == wholeLinesSelection.end) {
+                paragraphs.add(new Selection(startOfParagraph, endOfParagraph));
+
+                if (endOfParagraph < wholeLinesSelection.end) {
+                    startOfParagraph = endOfParagraph + 1;
+                } else {
+                    break;
+                }
+            }
+            ++endOfParagraph;
+        }
+
+        return paragraphs;
+    }
+
+    public Selection extendToFullLines(CharSequence str) {
+        int startOfFirstParagraph = this.start;
+        while (startOfFirstParagraph > 0 && str.charAt(startOfFirstParagraph - 1) != '\n') {
+            --startOfFirstParagraph;
+        }
+
+        int endOfLastParagraph = this.end;
+        while(endOfLastParagraph < str.length() - 1 && str.charAt(endOfLastParagraph) != '\n') {
+            ++endOfLastParagraph;
+        }
+
+        return new Selection(startOfFirstParagraph, endOfLastParagraph);
+    }
+
     public boolean isCollapsed() {
         return (start == end);
     }
 
     public void apply(EditText editor) {
         editor.setSelection(start, end);
+    }
+
+    public String toString() {
+        return "start: " + start + ", end: " + end;
     }
 }
---
+++ widgets/src/test/java/ro/kenjiru/ui/widgets/SelectionTest.java
@@ -0,0 +1,49 @@
+package ro.kenjiru.ui.widgets;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import ro.kenjiru.ui.widgets.richtexteditor.Selection;
+
+import static org.junit.Assert.*;
+
+public class SelectionTest {
+    @Test
+    public void extendToFullLines() throws Exception {
+        CharSequence str = "Lorem ipsum\n dolor sit amet,\nconsectetur adipiscing elit.";
+        Selection selection = new Selection(20, 40);
+
+        Selection fullLinesSelection = selection.extendToFullLines(str);
+        assertEquals(12, fullLinesSelection.start);
+        assertEquals(56, fullLinesSelection.end);
+    }
+
+    @Test
+    public void extendToFullLinesCollapsed() throws Exception {
+        CharSequence str = "Lorem ipsum\n dolor sit amet,\nconsectetur adipiscing elit.";
+        Selection selection = new Selection(5, 5);
+
+        Selection fullLinesSelection = selection.extendToFullLines(str);
+        assertEquals(0, fullLinesSelection.start);
+        assertEquals(11, fullLinesSelection.end);
+    }
+
+    @Test
+    public void getParagraphsMultiLineSelection() throws Exception {
+        CharSequence str = "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\nEtiam justo velit, elementum";
+        Selection selection = new Selection(40, 70);
+
+        List<Selection> paragraphs = selection.getParagraphsInSelection(str);
+        assertEquals(2, paragraphs.size());
+    }
+
+    @Test
+    public void getParagraphsCollapsedSelection() throws Exception {
+        CharSequence str = "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\nEtiam justo velit, elementum";
+        Selection selection = new Selection(5, 5);
+
+        List<Selection> paragraphs = selection.getParagraphsInSelection(str);
+        assertEquals(1, paragraphs.size());
+    }
+}
